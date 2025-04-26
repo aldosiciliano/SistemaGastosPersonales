95d6829845548f78304d115b6a9393f614462088
Santi Calderon <scaldera404@gmail.com>
2025-04-26 20:16:19 -0300

    InterfazPrincipal (Incompleto)

---
+++ SistemasGastosPersonales/SistemasGastosPersonales/Formularios/InterfazPrincipal.vb
@@ -0,0 +1,45 @@
+﻿Public Class InterfazPrincipal
+    Private Sub InterfazPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub lblBalance_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+
+    Private Sub Guna2ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbxTipo.SelectedIndexChanged
+
+    End Sub
+
+    Private Sub btn_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Guna2DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles Guna2DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Guna2RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles rbtnDia.CheckedChanged
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs)
+
+    End Sub
+
+    Private Sub Guna2RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles Guna2RadioButton3.CheckedChanged
+
+    End Sub
+
+    Private Sub Guna2RadioButton1_CheckedChanged_1(sender As Object, e As EventArgs) Handles Guna2RadioButton1.CheckedChanged
+
+    End Sub
+
+    Private Sub Guna2RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles Guna2RadioButton2.CheckedChanged
+
+    End Sub
+
+    Private Sub Guna2ShadowPanel2_Paint(sender As Object, e As PaintEventArgs)
+
+    End Sub
+End Class
